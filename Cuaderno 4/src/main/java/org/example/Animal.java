63672cdf81f7a8a0819c3ecd2fe88ef708d3a798
Jorge Calvo <144229781+JorgeCalvoFernandezChacon@users.noreply.github.com>
2026-03-18 17:17:48 +0100

    Delete Cuaderno 4/src/main/java/org/example directory

--- Cuaderno 4/src/main/java/org/example/Animal.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-abstract class Animal {
-    private String name;
-    public Animal(String name) {
-        this.name = name;
-    }
-    public abstract void greets();
-}
--- Cuaderno 4/src/main/java/org/example/Resizable.java
+++
@@ -1,5 +0,0 @@
-package org.example;
-
-public interface Resizable {
-    public void resize(int percent);
-}
